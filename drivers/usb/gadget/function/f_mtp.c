dbec319aa28cb4e0c3051c1dfe6b6ffb2b6923a2
ChandanaKishori Chiluveru <cchilu@codeaurora.org>
2015-11-13 11:25:52 +0530

    usb: gadget: f_mtp: Fix mtp enumeration failure issue
    If cable is disconnected while send_file work is in progress,
    Endpoints will be disabled and state will be marked as OFFLINE.
    As a part of endpoint disable pending requests will be retire out
    with -ECONNRESET error. Due to this we will modify the device state
    as ERROR in completion callback. If the state is not offline then
    we will mark the device state to ready in send_file_work. This results
    in mtp driver to not returning any error for next mtp_write operation
    and mtp release will not be called.
    
    Hence fix this issue by adding a proper check in completions to not
    moving device state to ERROR in case of disconnect.
    
    Change-Id: Idb4e075c89bdf94790a321bc464d30eba546eeaa
    Signed-off-by: ChandanaKishori Chiluveru <cchilu@codeaurora.org>

--- drivers/usb/gadget/function/f_mtp.c
+++ drivers/usb/gadget/function/f_mtp.c
@@ -534,7 +534,7 @@ static void mtp_complete_in(struct usb_ep *ep, struct usb_request *req)
 {
 	struct mtp_dev *dev = _mtp_dev;
 
-	if (req->status != 0)
+	if (req->status != 0 && dev->state != STATE_OFFLINE)
 		dev->state = STATE_ERROR;
 
 	mtp_req_put(dev, &dev->tx_idle, req);
@@ -547,7 +547,7 @@ static void mtp_complete_out(struct usb_ep *ep, struct usb_request *req)
 	struct mtp_dev *dev = _mtp_dev;
 
 	dev->rx_done = 1;
-	if (req->status != 0)
+	if (req->status != 0 && dev->state != STATE_OFFLINE)
 		dev->state = STATE_ERROR;
 
 	wake_up(&dev->read_wq);
@@ -557,7 +557,7 @@ static void mtp_complete_intr(struct usb_ep *ep, struct usb_request *req)
 {
 	struct mtp_dev *dev = _mtp_dev;
 
-	if (req->status != 0)
+	if (req->status != 0 && dev->state != STATE_OFFLINE)
 		dev->state = STATE_ERROR;
 
 	mtp_req_put(dev, &dev->intr_idle, req);
@@ -676,7 +676,7 @@ static ssize_t mtp_read(struct file *fp, char __user *buf,
 		return -EINVAL;
 	}
 
-	DBG(cdev, "mtp_read(%zu)\n", count);
+	DBG(cdev, "%s(%zu) state:%d\n", __func__, count, dev->state);
 
 	/* we will block until we're online */
 	DBG(cdev, "mtp_read: waiting for online state\n");
@@ -771,7 +771,7 @@ done:
 		dev->state = STATE_READY;
 	spin_unlock_irq(&dev->lock);
 
-	DBG(cdev, "mtp_read returning %zd\n", r);
+	DBG(cdev, "%s returning %zd state:%d\n", __func__, r, dev->state);
 	return r;
 }
 
@@ -791,7 +791,7 @@ static ssize_t mtp_write(struct file *fp, const char __user *buf,
 		return -EINVAL;
 	}
 
-	DBG(cdev, "mtp_write(%zu)\n", count);
+	DBG(cdev, "%s(%zu) state:%d\n", __func__, count, dev->state);
 
 	spin_lock_irq(&dev->lock);
 	if (dev->state == STATE_CANCELED) {
@@ -830,6 +830,8 @@ static ssize_t mtp_write(struct file *fp, const char __user *buf,
 			((req = mtp_req_get(dev, &dev->tx_idle))
 				|| dev->state != STATE_BUSY));
 		if (!req) {
+			DBG(cdev, "%s request NULL ret:%d state:%d\n",
+				__func__, ret, dev->state);
 			r = ret;
 			break;
 		}
@@ -868,7 +870,7 @@ static ssize_t mtp_write(struct file *fp, const char __user *buf,
 		dev->state = STATE_READY;
 	spin_unlock_irq(&dev->lock);
 
-	DBG(cdev, "mtp_write returning %zd\n", r);
+	DBG(cdev, "%s returning %zd state:%d\n", __func__, r, dev->state);
 	return r;
 }
 
@@ -929,6 +931,9 @@ static void send_file_work(struct work_struct *data)
 			break;
 		}
 		if (!req) {
+			DBG(cdev,
+				"%s request NULL ret:%d state:%d\n", __func__,
+				ret, dev->state);
 			r = ret;
 			break;
 		}
@@ -986,7 +991,7 @@ static void send_file_work(struct work_struct *data)
 	if (req)
 		mtp_req_put(dev, &dev->tx_idle, req);
 
-	DBG(cdev, "send_file_work returning %d\n", r);
+	DBG(cdev, "%s returning %d state:%d\n", __func__, r, dev->state);
 	/* write the result */
 	dev->xfer_result = r;
 	smp_wmb();
@@ -1150,8 +1155,10 @@ static long mtp_send_receive_ioctl(struct file *fp, unsigned int code,
 	struct work_struct *work;
 	int ret = -EINVAL;
 
-	if (mtp_lock(&dev->ioctl_excl))
+	if (mtp_lock(&dev->ioctl_excl)) {
+		DBG(dev->cdev, "ioctl returning EBUSY state:%d\n", dev->state);
 		return -EBUSY;
+	}
 
 	spin_lock_irq(&dev->lock);
 	if (dev->state == STATE_CANCELED) {
@@ -1331,8 +1338,10 @@ fail:
 static int mtp_open(struct inode *ip, struct file *fp)
 {
 	printk(KERN_INFO "mtp_open\n");
-	if (mtp_lock(&_mtp_dev->open_excl))
+	if (mtp_lock(&_mtp_dev->open_excl)) {
+		pr_err("%s mtp_release not called returning EBUSY\n", __func__);
 		return -EBUSY;
+	}
 
 	/* clear any error condition */
 	if (_mtp_dev->state != STATE_OFFLINE)
@@ -1540,6 +1549,7 @@ mtp_function_bind(struct usb_configuration *c, struct usb_function *f)
 		mtp_ssp_out_comp_desc.bMaxBurst = max_burst;
 	}
 
+	fi_mtp->func_inst.f = &dev->function;
 	DBG(cdev, "%s speed %s: IN/%s, OUT/%s\n",
 		gadget_is_superspeed(c->cdev->gadget) ? "super" :
 		(gadget_is_dualspeed(c->cdev->gadget) ? "dual" : "full"),
@@ -1551,9 +1561,10 @@ static void
 mtp_function_unbind(struct usb_configuration *c, struct usb_function *f)
 {
 	struct mtp_dev	*dev = func_to_mtp(f);
+	struct mtp_instance *fi_mtp;
 	struct usb_request *req;
 	int i;
-
+	fi_mtp = container_of(f->fi, struct mtp_instance, func_inst);
 	mtp_string_defs[INTERFACE_STRING_INDEX].id = 0;
 	while ((req = mtp_req_get(dev, &dev->tx_idle)))
 		mtp_request_free(req, dev->ep_in);
@@ -1567,6 +1578,7 @@ mtp_function_unbind(struct usb_configuration *c, struct usb_function *f)
 	spin_unlock_irq(&dev->lock);
 	kfree(f->os_desc_table);
 	f->os_desc_n = 0;
+	fi_mtp->func_inst.f = NULL;
 }
 
 static int mtp_function_set_alt(struct usb_function *f,
