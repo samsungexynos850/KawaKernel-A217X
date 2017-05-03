72921298402d6a9119755a594e79c37f16f033cb
Liangliang Lu <luliang@codeaurora.org>
2017-05-03 16:06:35 +0800

    usb: gadget: mtp: Move mutex_init() to alloc_inst_mtp_ptp()
    Now mutex_init() is in function_alloc_mtp_ptp() which will be
    called when USB composition change.
    
    In the corner case, USB composition change when do mtp_read(),
    the mutex lock is initialized when do mutex_lock().
    
    Change-Id: I7a61f6fe6d8865462c5445f7075722f4f151b03f
    Signed-off-by: Liangliang Lu <luliang@codeaurora.org>

--- drivers/usb/gadget/function/f_mtp.c
+++ drivers/usb/gadget/function/f_mtp.c
@@ -1953,6 +1953,8 @@ struct usb_function_instance *alloc_inst_mtp_ptp(bool mtp_config)
 	usb_os_desc_prepare_interf_dir(&fi_mtp->func_inst.group, 1,
 					descs, names, THIS_MODULE);
 
+	mutex_init(&fi_mtp->dev->read_mutex);
+
 	return  &fi_mtp->func_inst;
 }
 EXPORT_SYMBOL_GPL(alloc_inst_mtp_ptp);
@@ -2046,7 +2048,6 @@ struct usb_function *function_alloc_mtp_ptp(struct usb_function_instance *fi,
 	dev->function.free_func = mtp_free;
 	fi->f = &dev->function;
 
-	mutex_init(&dev->read_mutex);
 	return &dev->function;
 #endif
 }
