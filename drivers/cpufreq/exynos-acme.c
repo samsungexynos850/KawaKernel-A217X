7b92debe715f37e159eaca95a77203a4c64d425e
Joel Fernandes <joelaf@google.com>
2017-05-18 22:41:33 -0700

    FROMLIST: cpufreq: Make iowait boost a policy option and enable it
    Make iowait boost a cpufreq policy option and enable it for intel_pstate
    cpufreq driver. Governors like schedutil can use it to determine if
    boosting for tasks that wake up with p->in_iowait set is needed.
    
    [diepquynh]: Disable iowait boost for 9810 by default
    
    Bug: 38010527
    Link: https://lkml.org/lkml/2017/5/19/43
    Change-Id: Icf59e75fbe731dc67abb28fb837f7bb0cd5ec6cc
    Signed-off-by: Joel Fernandes <joelaf@google.com>
    Signed-off-by: Diep Quynh <remilia.1505@gmail.com>

--- drivers/cpufreq/exynos-acme.c
+++ drivers/cpufreq/exynos-acme.c
@@ -314,6 +314,7 @@ static int exynos_cpufreq_driver_init(struct cpufreq_policy *policy)
 	policy->cur = get_freq(domain);
 	policy->cpuinfo.transition_latency = TRANSITION_LATENCY;
 	policy->dvfs_possible_from_any_cpu = true;
+	policy->iowait_boost_enable = true;
 	cpumask_copy(policy->cpus, &domain->cpus);
 
 	pr_info("CPUFREQ domain%d registered\n", domain->id);
