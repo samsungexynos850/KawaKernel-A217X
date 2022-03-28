cb4a1f04b859b10521163b1d472f7bc7008bd754
DozNaka <dozdguide@gmail.com>
2022-03-28 16:42:21 -0400

    build_kernel.sh: adapt to proton-clang
    Because of the patches for kernel version 4.19.148, compiling with the previous toolchain is no longer compatible and therefore a move to proton-clang is required.

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-export CROSS_COMPILE=$HOME/toolchains/gcc/bin/aarch64-linux-android-
-export CC=$HOME/toolchains/clang/bin/clang
-export CLANG_TRIPLE=$HOME/toolchains/gcc-linaro/aarch64-linux-gnu-
+# export CROSS_COMPILE=$HOME/toolchains/gcc/bin/aarch64-linux-android-
+# export CC=$HOME/toolchains/clang/bin/clang
+export CLANG_TRIPLE=$HOME/toolchains/gcc-linaro/bin/aarch64-linux-gnu-
+
+export LLVM=1
+export PATH="$HOME/toolchains/proton-clang/bin:$PATH"
+export CC=clang
+export CROSS_COMPILE=aarch64-linux-gnu-
 
 read -p "Clean source? [N] (Y/N): " clean_confirm
 if [[ $clean_confirm == [yY] || $clean_confirm == [yY][eE][sS] ]]; then
