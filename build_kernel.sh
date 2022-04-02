a2086065d5b0071717f3e5b6d7db0d269b51e6a6
DozNaka <dozdguide@gmail.com>
2022-04-02 18:14:40 -0400

    build_kernel: Delete out folder when cleaning

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 
-# export CROSS_COMPILE=$HOME/toolchains/gcc/bin/aarch64-linux-android-
-# export CC=$HOME/toolchains/clang/bin/clang
-export CLANG_TRIPLE=$HOME/toolchains/gcc-linaro/bin/aarch64-linux-gnu-
+clear
+export PLATFORM_VERSION=11
+export ANDROID_MAJOR_VERSION=r
+export ARCH=arm64
 
 export LLVM=1
 export PATH="$HOME/toolchains/proton-clang/bin:$PATH"
 export CC=clang
 export CROSS_COMPILE=aarch64-linux-gnu-
+export CLANG_TRIPLE=$HOME/toolchains/gcc-linaro/bin/aarch64-linux-gnu-
 
 read -p "Clean source? [N] (Y/N): " clean_confirm
 if [[ $clean_confirm == [yY] || $clean_confirm == [yY][eE][sS] ]]; then
     echo "Cleaning source ..."
     make clean && make mrproper
+    rm -rf $(pwd)/out
 else
     echo "Source will not be cleaned for this build."
 fi
+clear
 
-export PLATFORM_VERSION=11
-export ANDROID_MAJOR_VERSION=r
-export ARCH=arm64
 make -j64 -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y physwizz_defconfig
 make -j64 -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y
