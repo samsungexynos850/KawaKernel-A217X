a25373410d5bde466f9916705b933bf2a63be373
DozNaka <dozdguide@gmail.com>
2022-04-09 11:43:30 -0400

    Add support for A217F

--- build_kernel.sh
+++ build_kernel.sh
@@ -10,6 +10,9 @@ export PATH="$HOME/toolchains/proton-clang/bin:$PATH"
 export CC=clang
 export CROSS_COMPILE=aarch64-linux-gnu-
 export CLANG_TRIPLE=$HOME/toolchains/gcc-linaro/bin/aarch64-linux-gnu-
+export DEFCONFIG_LOC=$(pwd)/arch/$ARCH/configs
+
+rm -rf $DEFCONFIG_LOC/.tmp_defconfig
 
 read -p "Clean source? [N] (Y/N): " clean_confirm
 if [[ $clean_confirm == [yY] || $clean_confirm == [yY][eE][sS] ]]; then
@@ -21,5 +24,20 @@ else
 fi
 clear
 
-make -j64 -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y physwizz_defconfig
+read -p $'Choose device:\x0a1) SM-A217M\x0a2) SM-A217F\x0aSelection: ' device_selection
+if [[ $device_selection == 1 ]]; then
+    clear
+    echo "Selected SM-A217M"
+    cat $DEFCONFIG_LOC/physwizz_defconfig > $DEFCONFIG_LOC/.tmp_defconfig
+    cat $DEFCONFIG_LOC/a217m_defconfig >> $DEFCONFIG_LOC/.tmp_defconfig
+elif [[ $device_selection == 2 ]]; then
+    clear
+    echo "Selected SM-A217F"
+    cat $DEFCONFIG_LOC/physwizz_defconfig > $DEFCONFIG_LOC/.tmp_defconfig
+else
+    clear
+    echo $'You have not selected a valid device!\x0aQuit'
+fi
+
+make -j64 -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y .tmp_defconfig
 make -j64 -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y
