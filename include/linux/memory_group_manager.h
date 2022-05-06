ab0250faffb031234f1558ad4d82be4c6766ad6c
DozNaka <dozdguide@gmail.com>
2022-05-06 13:23:30 -0400

    GPU: Import R30P0 driver with KawaKernel changes

--- include/linux/memory_group_manager.h
+++ include/linux/memory_group_manager.h
@@ -1,11 +1,12 @@
+/* SPDX-License-Identifier: GPL-2.0 */
 /*
  *
- * (C) COPYRIGHT 2019 ARM Limited. All rights reserved.
+ * (C) COPYRIGHT 2019-2020 ARM Limited. All rights reserved.
  *
  * This program is free software and is provided to you under the terms of the
  * GNU General Public License version 2 as published by the Free Software
  * Foundation, and any use by you of this program is subject to the terms
- * of such GNU licence.
+ * of such GNU license.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -15,8 +16,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, you can access it online at
  * http://www.gnu.org/licenses/gpl-2.0.html.
- *
- * SPDX-License-Identifier: GPL-2.0
  *
  */
 
@@ -101,8 +100,8 @@ struct memory_group_manager_ops {
 	 *         imported memory.
 	 */
 	int (*mgm_get_import_memory_id)(
-			struct memory_group_manager_device *mgm_dev,
-			struct memory_group_manager_import_data *import_data);
+		struct memory_group_manager_device *mgm_dev,
+		struct memory_group_manager_import_data *import_data);
 
 	/**
 	 * mgm_update_gpu_pte - Modify a GPU page table entry for a memory group
@@ -150,9 +149,9 @@ struct memory_group_manager_ops {
 	 *         table entry was successfully installed.
 	 */
 	vm_fault_t (*mgm_vmf_insert_pfn_prot)(
-			struct memory_group_manager_device *mgm_dev, int group_id,
-			struct vm_area_struct *vma, unsigned long addr,
-			unsigned long pfn, pgprot_t pgprot);
+		struct memory_group_manager_device *mgm_dev, int group_id,
+		struct vm_area_struct *vma, unsigned long addr,
+		unsigned long pfn, pgprot_t pgprot);
 };
 
 /**
