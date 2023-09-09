bf5e2f282e55ebe3bc61c8894fe0e9447326a637
DozNaka <dozdguide@gmail.com>
2023-09-09 00:12:25 +0000

    Revert "fs/proc: hide magisk mounts for IsolatedService"
    This reverts commit 7206878df0a640fc84a3c05fb39b9b038b4a6fa3.

--- fs/proc_namespace.c
+++ fs/proc_namespace.c
@@ -94,19 +94,6 @@ static void show_type(struct seq_file *m, struct super_block *sb)
 	}
 }
 
-static inline int skip_magisk_entry(const char *devname)
-{
-	if (devname && strstr(devname, "magisk")) {
-		char name[TASK_COMM_LEN];
-		get_task_comm(name, current);
-		if (strstr(name, "Binder") ||
-		    strstr(name, "JavaBridge")) {
-			return SEQ_SKIP;
-		}
-	}
-	return 0;
-}
-
 static int show_vfsmnt(struct seq_file *m, struct vfsmount *mnt)
 {
 	struct proc_mounts *p = m->private;
@@ -120,9 +107,6 @@ static int show_vfsmnt(struct seq_file *m, struct vfsmount *mnt)
 		if (err)
 			goto out;
 	} else {
-		err = skip_magisk_entry(r->mnt_devname);
-		if (err)
-			goto out;
 		mangle(m, r->mnt_devname ? r->mnt_devname : "none");
 	}
 	seq_putc(m, ' ');
@@ -161,9 +145,6 @@ static int show_mountinfo(struct seq_file *m, struct vfsmount *mnt)
 		if (err)
 			goto out;
 	} else {
-		err = skip_magisk_entry(r->mnt_devname);
-		if (err)
-			goto out;
 		seq_dentry(m, mnt->mnt_root, " \t\n\\");
 	}
 	seq_putc(m, ' ');
@@ -228,9 +209,6 @@ static int show_vfsstat(struct seq_file *m, struct vfsmount *mnt)
 		if (err)
 			goto out;
 	} else {
-		err = skip_magisk_entry(r->mnt_devname);
-		if (err)
-			goto out;
 		if (r->mnt_devname) {
 			seq_puts(m, "device ");
 			mangle(m, r->mnt_devname);
